f4a44bee22ae5f62866b29d12e42163b6df88ee9
Cleemzy <razafintsalama.rmh@gmail.com>
2021-10-25 21:14:37 +0300

    added scheduler to sync genserver

--- lib/bebemayotte/sync_context/sync_block_worker.ex
+++ lib/bebemayotte/sync_context/sync_block_worker.ex
@@ -7,12 +7,26 @@ defmodule Bebemayotte.SyncBlockWorker do
   end
 
   def init(state) do
+    schedule_sync()
+    {:ok, state}
+  end
+
+  def handle_info(:sync, state) do
+    do_sync()
+    schedule_sync()
+    {:noreply, state}
+  end
+
+  defp schedule_sync do
+    Process.send_after(self(), :sync, 60 * 1000)
+  end
+
+  defp do_sync do
     #MAIN BLOCK INSERTION
     SyncContext.insert_missing_produits()
     #MAIN BLOCK UPDATE
     SyncContext.update_produits_from_changesets()
     #MAIN BLOCK DELETION
     SyncContext.delete_exceeding_produits()
-    {:ok, state}
   end
 end
